dfe2be276fdbb654731a82801f509923583898ac
Dilli Basker <dillibasker1@gmail.com>
2025-04-08 12:07:23 +0530

    fc

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Button from './Button';
+import { ethers ,Contract} from "ethers"
 import '../styles/components.css';
 
 const Navbar = () => {
   const [signer,setSigner]=useState(null)
   const [provider,setprovider]=useState(null)
+  const [contract,setContract]=useState(null)
+  const [account,setAccount]=useState(null)
 
   const contractAddress=""
   const contractAbi=[]
@@ -13,10 +16,29 @@ const Navbar = () => {
   useEffect(()=>{
     const connectWallet=async()=>{
       try{
-        const provider=new ethers.Browser
+        const provider=new ethers.BrowserPrivider(window.ethereum)
+        setprovider(provider)
+
+        const signer=await provider.getSigner();
+        setSigner(signer)
+
+        const address=await signer.getAddress();
+        setAccount(address);
+
+        console.log("address is :",address)
+        
+        const contract=new Contract(contractAddress,contractAbi,signer)
+        setContract(contrat)
+
       }
+
+      catch(err){
+        console.log("Please install metamask",err)
+        alert("Please install metamsk")
     }
-  })
+    }
+    connectWallet();
+  },[])
 
 
   return (
