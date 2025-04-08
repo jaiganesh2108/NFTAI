8c4a9e477ae613c300457ffc08a352d23b3937a3
Dilli Basker <dillibasker1@gmail.com>
2025-04-08 12:00:43 +0530

    fc

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Button from './Button';
 import '../styles/components.css';
 
 const Navbar = () => {
+  const [signer,setSigner]=useState(null)
+  const [provider,setprovider]=useState(null)
+
+  const contractAddress=""
+  const contractAbi=[]
+
+  useEffect(()=>{
+    const connectWallet=async()=>{
+      try{
+        const provider=new ethers.Browser
+      }
+    }
+  })
+
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
