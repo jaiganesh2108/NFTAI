e975b09f8fe1e3e598e61768bf25cde3e298cb58
Dilli Basker <dillibasker1@gmail.com>
2025-04-10 20:06:51 +0530

    l

--- Backend/server.js
+++ Backend/server.js
@@ -15,6 +15,8 @@ app.use(cors({
 app.use(bodyParser.json());
 dotenv.config()
 
+
+
 mongoose.connect(process.env.MONGO_URL).then(()=> console.log("Mongo DB is connect")).catch((err) => console.log("Mongo db is not connected",(err)))
 
 app.use("/api/upload", require("./Routes/routes"));
